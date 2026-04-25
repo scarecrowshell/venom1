ec71c228366657a2f6fed1332856109339af6489
1barnowl <angelware63@gmail.com>
2026-04-25 07:00:17 -0400

    Delete crowd_density_estimator directory

--- crowd_density_estimator/src/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/__init__.py
-from . import ingest
-from . import clean
-from . import process
-from . import visualize
-from . import cluster
-from . import utils
-from . import app
--- crowd_density_estimator/src/app.py
+++
@@ -1,7 +0,0 @@
-# src/app.py
-import streamlit as st
-import pandas as pd
-
-def run_app(df: pd.DataFrame):
-    st.title("Crowd Density Estimator")
-    st.write(df.head())
